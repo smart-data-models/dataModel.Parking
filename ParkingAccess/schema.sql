4c1ff398b75e54286d293c91e7d081606c52d5c3
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 13:53:14 +0200

    test of SQL export

--- ParkingAccess/schema.sql
+++ ParkingAccess/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model ParkingAccess of the subject dataModel.Parking for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE ParkingAccess_type AS ENUM ('ParkingAccess');
-CREATE TABLE ParkingAccess (address JSON, alternateName TEXT, areaServed TEXT, category JSON, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, features JSON, height NUMERIC, name TEXT, owner JSON, slope NUMERIC, source TEXT, type ParkingAccess_type, width NUMERIC);
+CREATE TABLE ParkingAccess (address JSON, alternateName TEXT, areaServed TEXT, category JSON, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, features JSON, height NUMERIC, id TEXT PRIMARY KEY, location JSON, name TEXT, owner JSON, seeAlso JSON, slope NUMERIC, source TEXT, type ParkingAccess_type, width NUMERIC);
